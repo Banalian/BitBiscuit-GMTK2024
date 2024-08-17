6d97c74d809cd85d0943e67a80c67e0a8df56756
Banalian <64379456+Banalian@users.noreply.github.com>
2024-08-16 23:49:23 -0400

    chore(data): change data representation for the ingredients

--- Scripts/System/ingredient.gd
+++ Scripts/System/ingredient.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name Ingredient
 
+# used_in is not important to set for additional ingredients
+@export var used_in : Constants.MixType
 @export var ingredient_type : Constants.IngredientType
 @export var ingredient_name := "Invalid"
 @export var ingredient_texture : Texture2D
